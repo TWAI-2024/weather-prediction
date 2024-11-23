765dc0b7985da1c66350f71fa9d789cd2c426093
Bianca-Marina Codau <codau_bianca@yahoo.com>
2024-11-23 23:58:38 +0100

    Added sampling option for dataset

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -3,18 +3,18 @@ from typing import Tuple, Optional
 import pandas as pd
 
 class Dataset:
-    def __init__(self, file_path: str, target_col: str):
+    def __init__(self, file_path: str, target_col: str, num_samples: Optional[int] = None):
         self.file_path = file_path
         self.target_col = target_col
+        self.num_samples = num_samples  
 
     def load_data_frame(self) -> pd.DataFrame:
         """
         :return: the full data frame for this dataset (including the class column)
         """
         data = pd.read_csv(self.file_path).dropna()
-        # TODO: just sample?
-        # if self.num_samples is not None:
-        #    df = df.sample(self.num_samples, random_state=self.random_seed)
+        if self.num_samples is not None:
+            df = df.sample(self.num_samples, random_state=self.random_seed)
         return data
     
     def load_xy(self) -> Tuple[pd.DataFrame, pd.Series]:
