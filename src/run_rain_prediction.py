6b740feea4e9d27036d83770d7b268abac8d6a39
Antonia Schmidt <schmidt66@uni-potsdam.de>
2024-11-28 09:51:26 +0100

    Add model metric visualisations

--- src/run_rain_prediction.py
+++ src/run_rain_prediction.py
@@ -9,29 +9,28 @@ from sklearn.metrics import classification_report
 from rainpred.model_factory import ModelFactory
 from utils.data_base import DataInfo
 from utils.dataset import Dataset
-from utils.load_kaggle_data import load_via_kaggle
+#from utils.load_kaggle_data import load_via_kaggle
 from rainpred.data import COL_RAINTOMORROW, RainDataCleaner
+from utils import visualisations
 
 np.random.seed(0)
 
 def main():
-    load_via_kaggle('jsphyg/weather-dataset-rattle-package')
-
+    #load_via_kaggle('jsphyg/weather-dataset-rattle-package')
+    plot_path = "results/plots/"
     dataset = Dataset(file_path="./data/weatherAUS.csv", target_col=COL_RAINTOMORROW)
 
     data = dataset.load_data_frame()
 
-    DataInfo.info(data)
-    DataInfo.analyze_unique_values(data)
+    # Print general data information
+    #DataInfo.info(data)
+    #DataInfo.analyze_unique_values(data)
 
     # Data Cleaning
 
     data = RainDataCleaner.clean_data(data)
 
     X, y = data.drop(columns=COL_RAINTOMORROW), data[COL_RAINTOMORROW]
-
-    # TODO: Include feature engineering
-    # Feature Engineering 
 
     # Splitting test and training sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -55,8 +54,11 @@ def main():
         print(f"Score of {name}:", score)
         # Predicting the test set results
         y_pred = clf.predict(X_test)
-        # y_pred = (y_pred > 0.5)
+
         print(classification_report(y_test, y_pred))
+        visualisations.visualise_metrics(clf, name, plot_path, y_pred, y_test)
+        
+
 
 if __name__ == '__main__':
     main()
--- src/utils/visualisations.py
+++ src/utils/visualisations.py
@@ -1,20 +1,35 @@
-# confusion matrix
-#cmap1 = sns.diverging_palette(260,-10,s=50, l=75, n=5, as_cmap=True)
-#plt.subplots(figsize=(12,8))
-#cf_matrix = confusion_matrix(y_test, y_pred)
-#sns.heatmap(cf_matrix/np.sum(cf_matrix), cmap = cmap1, annot = True, annot_kws = {'size':15})
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import numpy as np
 
-# print loss curve and acc improvements    
-# MLPClassifier
-#val_acc = clf["model"].validation_scores_
+def plot_loss_curve(clf, name, path):
+    try:
+        loss_values = clf["model"].loss_curve_
+        fig, ax = plt.subplots()
+        ax.plot(loss_values)
+        fig.savefig(path + name + "_plot_loss_curve.png", dpi=300)
+    except AttributeError:
+        print('Attribute Error: No loss curve available for this model')
 
-#loss_values = clf["model"].loss_curve_
 
-def plot_loss_curve():
-    pass
+def plot_validation_scores(clf, name, path):
+    try:
+        val_acc = clf["model"].validation_scores_
+        fig, ax = plt.subplots()
+        ax.plot(val_acc)
+        fig.savefig(path + name + "_plot_validation_scores.png", dpi=300)
+    except AttributeError:
+        print('Attribute Error: No validation scores available for this model')
 
-def plot_validation_scores():
-    pass
 
-def plot_conf_matrix():
-    pass
+def plot_conf_matrix(y_test, y_pred, name, path):
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    fig = ConfusionMatrixDisplay(confusion_matrix=cf_matrix)
+    fig.plot().figure_.savefig(path + name + "_confusion_matrix.png")
+
+
+def visualise_metrics(clf, name, path, y_test, y_pred):
+    plot_loss_curve(clf, name, path)
+    plot_validation_scores(clf, name, path)
+    plot_conf_matrix(y_test, y_pred, name, path)
